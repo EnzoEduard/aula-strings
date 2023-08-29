9ee16ec616ee913a875ac6afc4c0c1a46c629a71
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-08-29 16:34:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+
+                                                                                                                                                                                                                                   // exercicio de interpretação de codigo;
+// 1: serazw impresso na tela o resultado f 9
+
+// 2: Sera impresso no console da seguinte maneira "Subi num ônibus em Mirrocos", mais a quantidade de caracteres que foram adicionados a frase.;
+
+// exercicio de escrita de codigo;
+
+/* 
+1
+ const email = prompt("digite seu email")
+ const nome = prompt("nome do  usuario");
+ console.log("O email " + email + " foi cadastrado com sucesso " + "seja bem vindo(a) " + nome);
+ */
+/*
+2
+ const comidas = ["galinha", "xis", "batata frita", "pipoca", "sorvete de mamao"];
+ console.log(comidas);
+ console.log("essas sao as minhas comidas favoritas");
+ console.log(comidas[0]);
+ console.log(comidas[1]);
+ console.log(comidas[2]);
+ console.log(comidas[3]);
+ console.log(comidas[4]);
+ */
+/*
+3
+ const listaDeTarefas = [];
+ const tarefas = prompt("tarefas para realizar:");
+ listaDeTarefas.push("lavar a louça");
+ listaDeTarefas.push("limpar a mesa");
+ listaDeTarefas.push("lavar as roupas"); 
+ console.log(listaDeTarefas);
+ const digite = Number(prompt("qual tarefa voce ja realizou? [0 a 2]")); 
+console.log(listaDeTarefas.splice (digite,1));
+ console.log(listaDeTarefas);
+ */
